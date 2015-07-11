041d29096c9e3413800ded093ca6ed59c0d4c5b1
devinivy <devin@bigroomstudios.com>
2015-07-11 00:25:23 -0400

    fix example linting errors.

--- example/policies/moonInPhase.js
+++ example/policies/moonInPhase.js
@@ -1,10 +1,10 @@
 var Wreck = require('wreck');
-var Boom  = require('boom');
+var Boom = require('boom');
 
 var moonInPhase = function (request, reply, next) {
- 
+
     var timestamp = parseInt(Date.now() / 1000);
-    
+
     Wreck.get('http://api.farmsense.net/v1/moonphases/?d=' + timestamp, { json: 'force' }, function (err, res, payload) {
 
         if (err || !payload || !payload[0] || payload[0].Error) {
@@ -13,14 +13,14 @@ var moonInPhase = function (request, reply, next) {
         }
 
         if (payload[0].Phase !== 'Waning Cresent') {
-            
+
             return next(null, false, payload[0].Phase + ' is the wrong moon phase for admin night.');
         }
 
         next(null, true);
     });
 
-}
+};
 
 moonInPhase.applyPoint = 'onPreHandler';
 
